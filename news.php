4d27d7e5bc22c76f0006f4e82384a6ae55cf4067
william wei <45888888+tlyse@users.noreply.github.com>
2024-04-18 00:53:55 +0800

    Add files via upload

---
+++ news.php
@@ -0,0 +1,128 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>新闻资讯 - POUDUN SANITARY WARE</title>
+<meta name="keywords" content="POUDUN SANITARY WARE,POUDUN官网,POUDUN陶瓷,智能坐便器,艺术盆,浴室柜,五金花洒"/>
+<meta name="description" content="本公司是一家卫生洁具、配件的开发、生产与销售为一体的综合性现代企业，是世界知名企业实力雄厚，不断引进先进意大利的生产技术设备，经验丰富的高素质技术骨干的管理人才。"/>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+<link rel="stylesheet" type="text/css" href="css/swiper.min.css"/>
+<link rel="stylesheet" type="text/css" href="css/animation.css" />
+<link rel="stylesheet" type="text/css" href="css/home.css@v=1713272143"/>
+<script src="js/swiper.min.js" type="text/javascript"></script>
+<script src="js/jquery-1.9.1.min.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body class="gray_bg" style="padding-top: 150px;">
+<div class="header_box header_color">
+	<div class="container clearfix rel">
+		<a class="logo" href="index.php" title="POUDUN sanitary ware">
+		</a>
+		
+		<nav class="fl">
+			<ul>
+				<li ><a href="index.php">首页</a></li>
+				<li ><a href="join.php">品牌加盟 </a></li>
+				<li ><a href="productlist.php">产品一览</a></li>
+			</ul>
+		</nav>
+		<nav class="fr">
+			<ul>
+				<li class="active"><a href="news.php">新闻资讯</a></li>
+				<li ><a href="about.php">公司简介</a></li>
+				<li ><a href="contact.php">联系我们</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+<div class="news-box">
+	<div class="container clearfix">
+		<div class="index-title text-center wow fadeinB">
+			<h2 class="black">news information</h2>
+			<h3 class="black">新闻资讯</h3>
+		</div>
+		<div class="news-list">
+			<ol class="clearfix">
+									<li class="radius wow fadeinB" data-wow-delay=".0s">
+						<a href="news_show.php@id=21" title="POUDUN官网于2022年9月20日正式上线！">
+							<div class="news-img"><div style="background-image: url(uploadimg/20220920163310_55411.jpg);"></div></div>
+							<div class="news-info">
+								<h3>POUDUN官网于2022年9月20日正式上线！</h3>
+								<p>2022年8月14日，POUDUN官方网站正式上线，目前新官网已经处于测试完善，欢迎各位广泛提供意见建议！...</p>
+							</div>
+							<div class="news-date">
+								<font>09-20</font>
+								<item></item>
+							</div>
+						</a>
+					</li>
+				
+									<li class="radius wow fadeinB" data-wow-delay=".1s">
+						<a href="news_show.php@id=23" title="POUDUN卫浴产品细节图展示">
+							<div class="news-img"><div style="background-image: url(uploadimg/20220920171618_68057.jpg);"></div></div>
+							<div class="news-info">
+								<h3>POUDUN卫浴产品细节图展示</h3>
+								<p>POUDUN产品从款式的设计到原材料的选择，从专业生产及抛光加工，再到质量检验均有严格的标准，每道工序一丝不苟，我们做到精益求精因此，产品具有样式新颖、抗击力强、环保节水、坚固耐用、防污耐蚀、易清洗等优质特性。...</p>
+							</div>
+							<div class="news-date">
+								<font>09-20</font>
+								<item></item>
+							</div>
+						</a>
+					</li>
+				
+				 
+			</ol>
+						<div class="expage">
+				<span class="curr"><<</span> <span class="curr"><</span> <span class="curr">1</span>
+ <span class="curr">></span> <span class="curr">>></span>			</div>
+					</div>
+	</div> 
+</div>
+<div class="footer">
+	<div class="foot-box">
+		<div class="container clearfix">
+			<div class="foot-logo fl">
+				<img src="images/logo.png" />
+			</div>
+			<div class="foot-info fl">
+				<h3>POUDUN sanitary ware</h3>
+				<p>					服务热线：400-879-3718&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;网站：www.POUDUN.com				</p>
+			</div>
+			<div class="foot-right fr">
+				<img src="images/qr.jpg" />
+				<p>扫一扫添加微信客服</p>
+			</div>
+		</div>
+	</div>
+	<div class="copy">
+		<div class="container clearfix">
+			<a class="fr" href="../www.waperp.com/default.htm" target="_blank">技术支持：微派科技</a>
+			COPYRIGHT © 2022 POUDUN SANITARY WARE ALL RIGHTS RESERVED		</div>
+	</div>
+</div>
+<!--[if !IE]><!-->
+<script src="js/wow.min.js" type="text/javascript"></script>
+<!--<![endif]-->
+<script type="text/javascript">
+$(window).on('load scroll',function(){
+	var scroll = $(window).scrollTop(); 
+	if (scroll >= 200) { 
+		$(".header_box").addClass('active');
+	} else{
+		$(".header_box").removeClass('active slideUp head_bg');
+	}
+	if (scroll >= 400) { 
+		$(".header_box").addClass('slidedowm slideUp');
+	} else{
+		$(".header_box").removeClass('slidedowm');
+	}
+})
+$(function(){
+	new WOW().init();
+})
+</script>
+  
+</body>
+</html>
